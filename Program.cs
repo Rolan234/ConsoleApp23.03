4fc52ee9c939bbd3567b81a4780c8a5c1ee799c3
Ahsim-Samish <s_bernaz@mail.ru>
2023-03-23 11:44:15 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Сортировка ОМ по возрастанию. 
+            int[] mass = { 12, -13, 14, 15, -17 };
+           
+            Console.Read();
+
+           
+            
+        }
+    }
+}
